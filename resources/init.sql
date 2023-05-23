71e3bfd8eebe8cbc0ccd647fa25cbc96fe0990a7
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-05-23 19:59:00 +0300

    feat(#5): merge the init sql query with v2 schema

--- resources/init.sql
+++ resources/init.sql
@@ -1,80 +1,84 @@
 CREATE SCHEMA IF NOT EXISTS public;
 
-CREATE TABLE "locations"
-(
-    "id"                   serial primary key,
-    "city_id"              int,
-    "city_ysk_id"          int,
-    "city_name"            varchar    NOT NULL,
-    "district_id"          int,
-    "district_ysk_id"      int,
-    "district_name"        varchar    NOT NULL,
-    "county_ysk_id"        int,
-    "county_name"          varchar,
-    "neighbourhood_id"     int unique NOT NULL,
+CREATE TABLE "locations" (
+    "id" serial primary key,
+    "city_ysk_id" int,
+    "city_name" varchar NOT NULL,
+    "district_id" int,
+    "district_ysk_id" int,
+    "district_name" varchar NOT NULL,
+    "county_ysk_id" int,
+    "county_name" varchar,
     "neighbourhood_ysk_id" int,
-    "neighbourhood_name"   varchar,
-    volunteer_data         int,
-    "threshold"            int
+    "neighbourhood_name" varchar,
+    volunteer_data int,
+    "threshold" int
 );
 
-ALTER TABLE "locations"
-    ADD CONSTRAINT "uq_locations_ysk_id" UNIQUE ("city_ysk_id", "district_ysk_id", "county_ysk_id",
-                                                 "neighbourhood_ysk_id");
+ALTER TABLE
+    "locations"
+ADD
+    CONSTRAINT "uq_locations_ysk_id" UNIQUE (
+        "city_ysk_id",
+        "district_ysk_id",
+        "county_ysk_id",
+        "neighbourhood_ysk_id"
+    );
 
 CREATE INDEX "idx_locations_neighbourhood_id" ON "locations" ("neighbourhood_id");
 
-CREATE TABLE "buildings"
-(
-    "id"          serial primary key,
-    "ysk_id"      int unique,
-    "name"        varchar NOT NULL,
-    "location_id" int     NOT NULL REFERENCES "locations" ("id")
+CREATE TABLE "buildings" (
+    "id" serial primary key,
+    "ysk_id" int unique,
+    "name" varchar NOT NULL,
+    "location_id" int NOT NULL REFERENCES "locations" ("id")
 );
 
-CREATE TABLE "sources"
-(
-    "id"   serial primary key,
-    "name" varchar unique NOT NULL
+CREATE TABLE "sources" (
+    "id" serial primary key,
+    "name" varchar unique NOT NULL,
+    "type" enum (
+        'ysk',
+        'volunteer',
+        'ballot_box',
+        'volunteer_location_count'
+    ) NOT NULL
 );
 
-CREATE TABLE "volunteers"
-(
-    "id"          serial primary key,
+CREATE TABLE "volunteers" (
+    "id" serial primary key,
     volunteer_doc jsonb,
     "building_id" int REFERENCES "buildings" ("id"),
     "location_id" int REFERENCES "locations" ("id"),
-    "confirmed"   boolean,
-    "source_id"   int REFERENCES "sources" ("id")
+    "confirmed" boolean,
+    "source_id" int REFERENCES "sources" ("id")
 );
 
-CREATE TABLE "ballot_boxes"
-(
-    "id"          serial primary key,
-    "box_no"      int,
-    "ysk_id"      int unique,
+CREATE TABLE "ballot_boxes" (
+    "id" serial primary key,
+    "box_no" int,
+    "ysk_id" int unique,
     "building_id" int not null REFERENCES "buildings" ("id"),
     "location_id" int not null REFERENCES "locations" ("id"),
     "voter_count" int
 );
 
-CREATE TABLE "users"
-(
-    "id"        serial primary key,
-    "username"  varchar unique,
-    "password"  varchar NOT NULL,
+CREATE TABLE "users" (
+    "id" serial primary key,
+    "login" varchar unique,
+    "passwd" varchar NOT NULL,
     "source_id" int REFERENCES "sources" ("id")
 );
 
-CREATE TABLE "volunteer_location_counts"
-(
-    "id"               serial primary key,
-    "count"            int,
-    "source_id"        int REFERENCES "sources" ("id"),
-    "building_id"      int REFERENCES "buildings" ("id"),
-    "location_id"      int unique REFERENCES "locations" ("id"),
+CREATE TABLE "volunteer_counts" (
+    "id" serial primary key,
+    "count" int,
+    "source_id" int REFERENCES "sources" ("id"),
+    "building_id" int REFERENCES "buildings" ("id"),
+    "location_id" int unique REFERENCES "locations" ("id"),
     "neighbourhood_id" int unique
 );
 
-CREATE INDEX "idx_volunteer_location_counts_neighbourhood_id" ON "volunteer_location_counts" ("neighbourhood_id");
-CREATE INDEX "idx_volunteer_location_counts_location_id" ON "volunteer_location_counts" ("location_id");
+CREATE INDEX "idx_volunteer_counts_neighbourhood_id" ON "volunteer_counts" ("neighbourhood_id");
+
+CREATE INDEX "idx_volunteer_counts_location_id" ON "volunteer_counts" ("location_id");
