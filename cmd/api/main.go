8137d41d612eeb9fd1a1d816f9ef9c333fea8a7e
Guray Gurkan <guraygrkn@gmail.com>
2023-05-25 07:50:15 +0300

    Feature/ovo data (#30)
    * ovo data release
    
    * api port changed to 7777
    
    * mock endpoints changed. Auth added. Cache after S3 store operation
    
    * compression enabled

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/acikkaynak/musahit-harita-backend/aws/s3"
 	"github.com/acikkaynak/musahit-harita-backend/middleware/auth"
 	"github.com/acikkaynak/musahit-harita-backend/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"os"
 	"os/signal"
 	"syscall"
@@ -75,13 +76,13 @@ func main() {
 	app.Use(auth.New())
 	app.Use(cache.New())
 
-	// gzip compression handled by nginx
-	//app.Use(compress.New(compress.Config{
-	//	Next: func(c *fiber.Ctx) bool {
-	//		return c.Path() == "/swagger/index.html"
-	//	},
-	//	Level: compress.LevelBestCompression,
-	//}))
+	// gzip compression can be handled by nginx
+	app.Use(compress.New(compress.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Path() == "/swagger/index.html"
+		},
+		Level: compress.LevelBestCompression,
+	}))
 
 	// register repositories to fiber app
 	pgStore := repository.New()
